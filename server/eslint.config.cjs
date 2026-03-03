6c763986a7f7863bb5c4072ad1a10cc3ece7b19c
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-03-03 00:13:21 -0800

    "feat: WIP setup for backend"

---
+++ server/eslint.config.cjs
@@ -0,0 +1,24 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+
+  prettier,
+];
