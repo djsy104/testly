05793929c1195e0f41ea53fa9b515f40f31b75a5
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-03-07 23:06:46 -0800

    feat: Create static login page

--- client/src/app/router.jsx
+++ client/src/app/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import ErrorPage from '../pages/ErrorPage.jsx';
 import Register from '../pages/Register.jsx';
-// import Login from '../pages/Login.jsx';
+import Login from '../pages/Login.jsx';
 import Dashboard from '../pages/Dashboard.jsx';
 // import App from './App.jsx';
 import AppShell from './AppShell.jsx';
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
 
   {
     path: '/login',
-    element: <Register />,
+    element: <Login />,
     errorElement: <ErrorPage />,
   },
 
--- client/src/features/auth/AuthHeader.jsx
+++
@@ -1,9 +0,0 @@
-function AuthHeader({ title }) {
-  return (
-    <>
-      <h2>{title}</h2>
-    </>
-  );
-}
-
-export default AuthHeader;
