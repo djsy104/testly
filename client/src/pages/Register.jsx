7f16f239c7752538b9a13c5bc9340c3357b438c8
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-03-07 20:38:49 -0800

    feat: Setup basic routing logic

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  return (
+    <>
+      <h1>Register Page</h1>
+    </>
+  );
+}
+
+export default App;
