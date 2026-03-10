a6582d49f369f99b8388a073b1ca46280f1f5203
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-03-09 22:07:15 -0700

    feat: Added navbar to login and register pages

--- client/src/app/AppShell.jsx
+++ client/src/app/AppShell.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '@/components/layout/Navbar';
 
 function AppShell() {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
