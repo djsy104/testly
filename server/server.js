31fe731da7d90e2c75c7ce0f481a25380795a3bb
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-03-09 22:51:41 -0700

    feat: WIP Auth API integration

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use('/api/auth', authLimiter);
 
 // For front-end hosts
 const corsOptions = {
-  origin: ['http://127.0.0.1:5173'],
+  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
 };
 app.use(cors(corsOptions));
 
